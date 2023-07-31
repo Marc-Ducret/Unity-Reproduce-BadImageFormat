5c82b0f7c3dbc274aedb22f526c5c63ad70620e7
Marc Ducret <marc.t.ducret@gmail.com>
2023-07-31 22:05:17 +0200

    reproduce BadImageFormat

---
+++ Assets/Scripts/TestType.cs
@@ -0,0 +1,11 @@
+﻿using MainAssembly;
+using TestPackage;
+using Unity.Entities;
+
+[assembly: RegisterGenericSystemType(typeof(GenericSystem<TestType>))]
+
+namespace MainAssembly {
+    public struct TestType {
+        public int value;
+    }
+}
---
+++ Packages/TestPackage/TestPackage/GenericSystem.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace TestPackage {
+    public partial struct GenericSystem<T> : ISystem where T : unmanaged {
+        public void OnUpdate(ref SystemState state) {
+            UnityEngine.Debug.Log($"Hello {typeof(T)}");
+        }
+    }
+}
