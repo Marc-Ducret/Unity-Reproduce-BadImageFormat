5cf7c179e740d541131a24ef1e89945dfaf5a80c
Marc Ducret <marc.t.ducret@gmail.com>
2023-10-06 16:13:36 +0200

    Update to entities 1.1.0-exp.1.

--- Packages/TestPackage/TestPackage/GenericSystem.cs
+++ Packages/TestPackage/TestPackage/GenericSystem.cs
@@ -1,5 +1,7 @@
 ﻿using Unity.Entities;
 
+[assembly: RegisterGenericSystemType(typeof(TestPackage.GenericSystem<int>))]
+
 namespace TestPackage {
     public partial struct GenericSystem<T> : ISystem where T : unmanaged {
         public void OnUpdate(ref SystemState state) {
